4a7302d1df745ad9fe2bce570aaa7d7b594eaaad
omr-abo-el-maaty <121355879+omr-abo-el-maaty@users.noreply.github.com>
2022-12-24 20:19:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_if_number_is_perfect
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("write the first number : ");
+            int f = int.Parse(Console.ReadLine());
+            Console.Write("write the second number :");
+            int s = int.Parse(Console.ReadLine());
+            Console.WriteLine("the perfect numbers : ");
+            for (; f <= s; f++)
+            {
+                int sum = 0;
+
+                for (int n = 1; n < f; n++)
+                {
+                    if (f % n == 0)
+                    {
+
+                        sum += n;
+                  
+                    }
+                   
+                }
+                if (sum == f)
+                {
+                    Console.WriteLine(f);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
